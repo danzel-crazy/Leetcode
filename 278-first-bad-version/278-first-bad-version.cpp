8cb1a636cf1c207417ffc7bfdd3ea9156f8aa4d0
danzel <70528759+danzel-crazy@users.noreply.github.com>
2022-07-04 02:04:37 +0800

    Time: 0 ms (100.00%), Space: 6 MB (22.36%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,16 +5,18 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         long mid = 0 , left = 1, right = n;
+        int ans = mid;
         while(left <= right){
             mid = (left + right)/2;
             if(isBadVersion(mid)){
+                ans = mid;
                 right = mid-1;
             }
             else{
-                if(isBadVersion(mid+1)) return mid+1;
+                // if(isBadVersion(mid+1)) return mid+1;
                 left = mid+1;
             }
         }
-        return mid;
+        return ans;
     }
 };
