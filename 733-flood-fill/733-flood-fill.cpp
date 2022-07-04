5800bb937dff1a59f14f14c3c4b01af90548d7ec
danzel <70528759+danzel-crazy@users.noreply.github.com>
2022-07-05 01:50:03 +0800

    Time: 24 ms (11.86%), Space: 13.8 MB (98.86%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,35 +1,26 @@
 class Solution {
+    vector<pair<int, int>> directions {{0,1}, {1,0}, {-1,0}, {0,-1}};
+    int srcColor;
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int origin = image[sr][sc],m=image.size(),n=image[0].size();;
-        queue <pair<int, int>> q;
-        q.push({sr,sc});
-        
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            image[row][col] = color;
-            
-            
-            if(row-1>=0){ 
-                if(image[row-1][col]!=color && image[row-1][col]==origin)
-                    q.push({row-1,col});                     
-            }
-            if(col-1>=0){
-                if( image[row][col-1]!=color && image[row][col-1]==origin)
-                    q.push({row,col-1});
-            }
-            if(row+1<m){ 
-                if(image[row+1][col]!=color && image[row+1][col]==origin)
-                    q.push({row+1,col});
-            }
-            if(col+1<n){
-                if(image[row][col+1]!=color && image[row][col+1]==origin)
-                    q.push({row,col+1});
-            }
+    void recur(vector<vector<int>>& image, int sr, int sc, int color) {
+        if (image[sr][sc] == srcColor) {
+            image[sr][sc] = color;
         }
         
+        for (auto& p : directions) {
+            int x = sr + p.first;
+            int y = sc + p.second;
+            if (x > -1 && x < image.size() && y > -1 && y < image[0].size() && image[x][y] == srcColor) {
+                recur(image, x, y, color);
+            }
+        }
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        srcColor = image[sr][sc];
+        if (color != srcColor) {
+            recur(image, sr, sc, color);
+        }
         return image;
     }
 };
