fa486be7dceac85c1d4c059da170516c641d609d
danzel <70528759+danzel-crazy@users.noreply.github.com>
2022-07-06 23:28:16 +0800

    Time: 11 ms (53.14%), Space: 13.6 MB (79.96%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+    int dp[1005] = {0};
     int minCostClimbingStairs(vector<int>& cost) {
-        int a=0;
-        int b=0,c;
-        for(int i=2;i<=cost.size();i++){
-            c=min(cost[i-1]+a,cost[i-2]+b);
-            b=a;
-            a=c;
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2; i <= cost.size(); i++){
+            if(dp[i-1] + cost[i-1] > dp[i-2] + cost[i-2]){
+                if(dp[i] > dp[i-2] + cost[i-2] || dp[i] == 0)
+                    dp[i] = dp[i-2] + cost[i-2];
+            }
+            else{
+                if(dp[i] > dp[i-1] + cost[i-1] || dp[i] == 0)
+                    dp[i] = dp[i-1] + cost[i-1];
+            }
+        
         }
-        return c;
+        
+        return dp[cost.size()];
     }
 };
