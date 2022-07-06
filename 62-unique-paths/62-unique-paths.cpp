c4306fc3f4af3830373ae6fcab843653f3ca89df
danzel <70528759+danzel-crazy@users.noreply.github.com>
2022-07-06 23:45:27 +0800

    Time: 3 ms (43.78%), Space: 6 MB (73.11%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    int dp[100][100] = {0};
-    
     int uniquePaths(int m, int n) {
-        dp[0][0] = 1;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n;j++){
-                if(i-1 >= 0){
-                    dp[i][j] += dp[i-1][j] ;
-                }
-                
-                if(j-1 >= 0){
-                    dp[i][j] += dp[i][j-1] ;
-                }
-            }
-        }
+        int N = m + n - 2;
+        int R = min(m, n) - 1;
+        double res = 1;
         
-        return dp[m-1][n-1];
+        for(int i = 1; i <= R; i++){
+            res = res * (N - R + i)/ i;
+        }
+        return (int)res;
     }
 };
