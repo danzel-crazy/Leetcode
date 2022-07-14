346b6b746371fc753ed8dc5f572a0c9a73aa5462
danzel <70528759+danzel-crazy@users.noreply.github.com>
2022-07-14 22:52:42 +0800

    Time: 2 ms (90.84%), Space: 6.6 MB (43.61%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        int bulls = 0, cows = 0;
-        map <char, int> a, b;
-        
-        for(int i = 0; i < secret.length(); i++){
-            if(secret[i] == guess[i]){
-                bulls += 1;
-            }
-            else{
-                a[secret[i]]++;
-                b[guess[i]]++;
-            }
-        }
-        
-        for(int i = 0; i < 10; i++){
-            cows += min(a['0' + i], b['0'+i]); 
-        }
-        
-        string ans = "";
-        ans += to_string(bulls);
-        ans += "A";
-        ans += to_string(cows);
-        ans += "B";
-        
-        return ans;
+        int cows(0), bulls(0), s[10] = {0}, g[10] = {0};
+
+	// count bulls
+	for(int i = 0; i < secret.size(); i++)
+		if(secret[i] == guess[i]) 
+			bulls++;
+		else
+			s[secret[i] - '0']++, g[guess[i] - '0']++;        
+
+	// count cows
+	for(int i = 0; i < 10; i++)
+		cows += min(s[i], g[i]);
+
+	return to_string(bulls) + "A" + to_string(cows) + "B";
     }
 };
