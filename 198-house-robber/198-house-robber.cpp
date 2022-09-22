edc0d6d333d6c240d7d70eb7a42762410c950952
danzel <70528759+danzel-crazy@users.noreply.github.com>
2022-09-23 01:57:55 +0800

    Time: 3 ms (55.18%), Space: 7.5 MB (95.29%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        int dp[n];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        for(int i = 2; i < nums.size(); i++){
+            if((i-3) >= 0){
+                dp[i] = max(dp[i-2], dp[i-3]) + nums[i];
+            }
+            else dp[i] = dp[i-2] + nums[i];
+        }
+        
+        return dp[n-1] > dp[n-2] ? dp[n-1] : dp[n-2];
+    }
+};
