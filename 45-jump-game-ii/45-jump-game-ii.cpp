54ecd63c1e99b2559ea93a78a65865d012dcffc7
danzel <70528759+danzel-crazy@users.noreply.github.com>
2022-09-24 23:19:26 +0800

    Time: 20 ms (83.10%), Space: 16.6 MB (80.49%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = size(nums);
-        vector<int> dp(n, 10001);
-        dp[n - 1] = 0;
-        
-        for(int i = n-2; i >= 0; i--){
-            for(int step = 1; step <= nums[i]; step++){
-                dp[i]= min(dp[i], 1 + dp[min(n-1, i+step)]);
-            }
+        int i = 0, step = 0, last = 0, max_reach = 0;
+        while(last < n -1){
+           max_reach = max(max_reach, i + nums[i]);  
+            if(i == last) {			  
+                last = max_reach;     
+                step++;                          
+            }            
+            i++; 
         }
         
-        return dp[0];
+        return step;
     }
 };
